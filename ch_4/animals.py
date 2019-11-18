e1923ca04e2cea6eefb2045952ee19a93e5f4667
Brandon Pietrzyk <motorcross106@gmail.com>
2019-11-18 11:40:24 -0500

    more files and updates

--- ch_4/animals.py
+++ ch_4/animals.py
@@ -1,4 +1,4 @@
-animals=['cat','dog','bunny']
+animals=['cat','dog','bunny','goat','horse','gold fish','snake']
 for animal in animals:
     print(f"A {animal.title()} would make a great pet!")
 print("\nAny one of these animals would make a great pet!")
---
+++ ch_4/slices.py
@@ -0,0 +1,14 @@
+animals=['cat','dog','bunny','goat','horse','gold fish','snake']
+for animal in animals:
+    print(f"A {animal.title()} would make a great pet!")
+print("\nAny one of these animals would make a great pet!")
+
+print("\nThe first three items in this list are:")
+print(animals[2:])
+
+print("\nThree items from the middle of the list are:")
+print(animals[2:5])
+
+print("\nThe last three items in the list are:")
+print(animals[4:7])
+
