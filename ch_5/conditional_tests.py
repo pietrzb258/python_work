a6fb6b5600e4d2ef9d0a909bb0954d109a83ea70
pietrzb258 <motorcross106@gmail.com>
2019-11-24 21:21:48 -0500

    chapter 5 docs. booleand and if statements.

---
+++ ch_5/conditional_tests.py
@@ -0,0 +1,39 @@
+car = 'subaru'
+print("Is car == 'subaru? I predict true.")
+print(car=='subaru')
+
+print("\nIs car == 'audi'? i predict false.")
+print(car == 'audi')
+
+age_0=18
+age_1=12
+print("\nIs the sun if the ages greater than 40? i predict no.")
+print((age_0+age_1)>(40))
+
+my_car='escape'
+print("\nIs my car == Escape? i predict true.")
+print(my_car=='escape')
+
+
+age_2=10
+print("\nIs your age greater than 10? i predict false.")
+print(age_2>10)
+
+name='Brandon'
+print("\nIs name==Josh? i predict false.")
+print(name=='josh')
+
+fruit='banana'
+print("\nIs fruit==apple? i predict false.")
+print(fruit=='apple')
+
+age_3=21
+print("\nIs age_3>=21? i predict true.")
+print(age_3>=21)
+
+print("\nIs age_3=21? i predict true.")
+print(age_3==21)
+
+bike_model='red rider'
+print("\nIs bike_model=='red rider'? i predict true.")
+print(bike_model=='red rider')
---
+++ ch_5/magic_numbers.py
@@ -0,0 +1,4 @@
+answer=17
+if answer != 42:
+    print("That is not a correct answer. Please try again!")
+    
